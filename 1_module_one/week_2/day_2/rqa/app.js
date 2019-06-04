fed2444b6ef642fa6ef77ad7a71e30bcd86ad357
meta-one <meta-one@meta-one.local>
2019-06-04 11:03:32 +0200

    added solution for buttons challenge

--- 1_module_one/week_2/day_2/rqa/app.js
+++ 1_module_one/week_2/day_2/rqa/app.js
@@ -1,22 +1,30 @@
-console.log("hello world");
-
 // https://developer.mozilla.org/en-US/docs/Web/Events
 
-
-function buttonAction(evt) { // implicit parameter
-    const target = evt.target || evt.srcElement;
-    target.classList.toggle("is-active");
-    console.log("clicked !", evt, target);
+function addBtnStyle(btn) { // implicit parameter
+    const previous = document.querySelector("#top_buttons .btn.is-active");
+    if (previous) previous.classList.remove("is-active");
+    btn.classList.add("is-active");
 }
 
-window.addEventListener("DOMContentLoaded", function() {
-    const btn = document.getElementById("my_btn");
+function start() {
     const btns = document.querySelectorAll("#top_buttons .btn");
-    btn.textContent += " modified";
-    console.log(btns);
+    const display = document.getElementById("display");
 
     for (let i = 0; i < btns.length; i += 1) {
-        btns[i].onclick = buttonAction; // the evt is attached here... onclick
-    }
 
-});
+        btns[i].onclick = function (evt) {
+
+            const target = evt.target || evt.srcElement;
+
+            addBtnStyle(target);
+
+            if (target.id === "btn_joke") display.innerHTML = "Dentist: 'This will hurt a little.'<br> Patient: 'OK.'<br>Dentist: 'I’ve been having an affair with your wife for a while now.'";
+
+            else if (target.id === "btn_fact") display.innerHTML = "In Switzerland it is illegal to own just one guinea pig. <br> This is because guinea pigs are social animals,<br> and they are considered victims of abuse if they are alone. <br>Why isn’t this a law everywhere?!";
+
+            else display.innerHTML = '<img src="./cat.gif"/>';
+        }
+    }
+}
+
+window.addEventListener("DOMContentLoaded", start);
