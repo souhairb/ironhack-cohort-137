a353fcbdee7b9e23252ea0292b7e2c81023dd1d6
meta-one <meta-one@meta-one.local>
2019-06-05 15:25:49 +0200

    minor changes

--- 1_module_one/week_2/day_3/2_input_events/inputs.js
+++ 1_module_one/week_2/day_3/2_input_events/inputs.js
@@ -3,16 +3,29 @@ function displayPlayerCount(count) {
     displayer.textContent = count;
 }
 
+function changeColor(value) {
+    document.getElementById("selected_color").textContent = value; // display color value
+    document.querySelector("body").style.background = value; // change body bg color
+}
+
 function listenRange(evt) {
     const target = evt.target || evt.srcElement;
     displayPlayerCount(target.value);
 }
 
+
+function listenColor(evt) {
+    const target = evt.target || evt.srcElement; // get current input
+    changeColor(target.value); // pass it's value to a changeColor function
+}
+
 function start() {
-    const range = document.getElementById("my_first_range");
-    const number = document.getElementById("my_first_number");
-    range.oninput = listenRange;
-    number.oninput = listenRange;
+    const range = document.getElementById("my_first_range"); // select html element
+    const number = document.getElementById("my_first_number"); // select html element
+    const color = document.getElementById("my_first_color"); // select html element
+    range.oninput = listenRange; // set event listeners
+    number.oninput = listenRange; // set event listeners
+    color.oninput = listenColor; // set event listeners
 }
 
 window.addEventListener("DOMContentLoaded", start);
