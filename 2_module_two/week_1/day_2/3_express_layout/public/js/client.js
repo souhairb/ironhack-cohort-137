4f3cb637d1a513a6e8161a0b3a7cf4f06be57b6d
meta-one <meta-one@meta-one.local>
2019-06-19 11:18:22 +0200

    small changes

--- 2_module_two/week_1/day_2/3_express_layout/public/js/client.js
+++ 2_module_two/week_1/day_2/3_express_layout/public/js/client.js
@@ -1,2 +1,10 @@
 console.log("DOM loaded, client is ready to rock !!!");
-console.log(document.querySelector("body"));
+
+const body = document.querySelector("body");
+
+console.log(body);
+
+window.onclick = function(evt) {
+    console.log("hey ya clicked me !!!");
+    body.classList.toggle("is-dark");
+};
